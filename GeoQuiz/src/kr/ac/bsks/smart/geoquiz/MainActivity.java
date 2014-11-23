10913364950300a4ba817f04675311f21f2801ad
macbookpromulti3rdfloor <macbookpro@192.168.0.13>
2014-11-23 15:56:27 +0900

    3차 commit

--- GeoQuiz/src/kr/ac/bsks/smart/geoquiz/MainActivity.java
+++ GeoQuiz/src/kr/ac/bsks/smart/geoquiz/MainActivity.java
@@ -6,23 +6,42 @@ import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
+	private Button mNextButton;
+	private TextView mQuestionTextView;
 	
+	private TrueFalse[] mQuestionBank = new TrueFalse[] { 
+			new TrueFalse( R.string.question_oceans, true ),
+			new TrueFalse( R.string.question_mideast, true ),
+			new TrueFalse( R.string.question_africa, true ),
+			new TrueFalse( R.string.question_americas, true ),
+			new TrueFalse( R.string.question_asia, true ),
+	} ;
+    
+	private int mCurrentIndex = 0;
 	
+	private void updateQuestion() {
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText( question );
+	}
 	
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mTrueButton = (Button) findViewById(R.id.true_button);
+		mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+		int question = mQuestionBank[mCurrentIndex].getQuestion();
+		mQuestionTextView.setText( question );
 		
+		
+		mTrueButton = (Button) findViewById(R.id.true_button);
 		mTrueButton.setOnClickListener( new OnClickListener() {
 			
 			@Override
@@ -42,6 +61,19 @@ public class MainActivity extends Activity {
 			}
 		});
 		
+		mNextButton = (Button) findViewById(R.id.next_button);
+		mNextButton.setOnClickListener( new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				mCurrentIndex = ( mCurrentIndex + 1) % mQuestionBank.length;
+				updateQuestion();
+			}
+		});
+		
+		updateQuestion();
+		 
 		
 	}
 
